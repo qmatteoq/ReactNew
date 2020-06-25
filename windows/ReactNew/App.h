47efa0cb93e231fc50d0e84fe3c0ad388e73a0f6
Matteo Pagani <matteo.pagani@microsoft.com>
2020-06-25 10:32:37 +0200

    First commit of the code

---
+++ windows/ReactNew/App.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::ReactNew::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::ReactNew::implementation
+
+
---
+++ windows/ReactNew/ReactPackageProvider.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "winrt/Microsoft.ReactNative.h"
+
+
+namespace winrt::ReactNew::implementation
+{
+    struct ReactPackageProvider : winrt::implements<ReactPackageProvider, winrt::Microsoft::ReactNative::IReactPackageProvider>
+    {
+    public: // IReactPackageProvider
+        void CreatePackage(winrt::Microsoft::ReactNative::IReactPackageBuilder const &packageBuilder) noexcept;
+    };
+} // namespace winrt::ReactNew::implementation
+
+
